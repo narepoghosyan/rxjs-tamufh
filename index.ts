a219e06a975a10f1af2049c3d22c5f4fed389cb0
narepoghosyan <nare1poghosyan@gmail.com>
2022-04-10 17:26:39 +0400

    1 file modified

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ import {
 } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
 
-// 1. From an Observable of strings...
+// 1. From an Observable of strings, console.log an array of lowercase characters from each string.
 
 const strings$ = of('Word', 'Another');
 
@@ -28,7 +28,7 @@ strings$
   )
   .subscribe(console.log);
 
-// 2. From an Observable of click events...
+// 2. From an Observable of click events, console.log an object with x/y coordinates of the click on the screen.
 
 const clickEvent$ = fromEvent(document, 'click');
 
@@ -36,14 +36,14 @@ clickEvent$
   .pipe(map((e: MouseEvent) => ({ x: e.clientX, y: e.clientY })))
   .subscribe(console.log);
 
-// 3. From an input...
+// 3. From an input, make an http call with a delay of 500ms after typing finished and console.log the result.
 
 const searchEvent$ = fromEvent(document.getElementById('searchinput'), 'input');
 
 searchEvent$
   .pipe(
     debounceTime(500),
-    filter((event) => event.target.value),
+    filter((event: MouseEvent) => event.target.value),
     switchMap((event) => {
       const value = event.target.value;
       const url = `https://api.mixcloud.com/search/?q=${value}&type=tag`;

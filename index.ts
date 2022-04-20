152defafba79977f675e76a559fd02ec49ac0848
narepoghosyan <nare1poghosyan@gmail.com>
2022-04-20 23:04:35 +0400

    2 files modified

--- index.ts
+++ index.ts
@@ -1,96 +1,36 @@
+import { from, fromEvent, interval, merge, of } from 'rxjs';
 import {
-  combineLatest,
-  concat,
-  filter,
-  forkJoin,
-  fromEvent,
-  map,
-  merge,
-  of,
-  timer,
-} from 'rxjs';
-import { fromFetch } from 'rxjs/fetch';
-import {
-  debounceTime,
-  defaultIfEmpty,
-  switchMap,
+  auditTime,
+  distinct,
+  mergeMap,
+  skipUntil,
   takeUntil,
-toArray,
+  toArray,
 } from 'rxjs/operators';
 
-// 1. Create a “you have been inactive popup” which will alert a window if user has not done anything with the mouse (click, double click, scroll, right click, mouse move) for 10 seconds​.
+// 1. Write a function that takes an array of numbers and returns an Observable of the same array, but without duplicate values​.
 
-merge(
-  fromEvent(document, 'click'),
-  fromEvent(document, 'dblclick'),
-  fromEvent(document, 'contextmenu'),
-  fromEvent(document, 'scroll'),
-  fromEvent(document, 'mousemove')
-)
-  .pipe(takeUntil(timer(10000)), defaultIfEmpty('inactive'))
-  .subscribe((val) => {
-    if (val === 'inactive') {
-      alert('you have been inactive');
-    }
-  });
+const removeDuplicates = (arr: number[]) => {
+  return from(arr).pipe(distinct(), toArray()).subscribe(console.log);
+};
 
-// 2. Create two number inputs and a dropdown which allows to select an arithmetic operation (+, -, *, /, %). After selecting any, display the result of the operation in the DOM​.
+removeDuplicates([1, 2, 3, 3, 4, 3, 7]);
 
-fromEvent(document.getElementById('select'), 'change')
+// 2.Create an Observable that shows the current time in the console, but only after a certain button has been clicked, and stops when another one is clicked​.
+
+interval(1000)
   .pipe(
-    switchMap((event) => {
-      const firstInput = document.getElementById('first') as HTMLInputElement;
-      const secondInput = document.getElementById('second') as HTMLInputElement;
-      const operator = event.target.value;
-      const operators = {
-        '+': (a: number, b: number) => a + b,
-        '-': (a: number, b: number) => a - b,
-        '*': (a: number, b: number) => a * b,
-        '/': (a: number, b: number) => a / b,
-        '%': (a: number, b: number) => a % b,
-      };
-      return concat(of(firstInput.value), of(secondInput.value)).pipe(
-        toArray(),
-        filter((data) => data[0] !== '' && data[1] !== ''),
-        map((data) => operators[operator](+data[0], +data[1]))
-      );
+    skipUntil(fromEvent(document.getElementById('start'), 'click')),
+    takeUntil(fromEvent(document.getElementById('stop'), 'click')),
+    mergeMap(() => {
+      const date = new Date();
+      return of(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
     })
   )
   .subscribe(console.log);
 
-// 3. Create three inputs in which the user can type a name of a country, call the “restcountries” api and display the country that has the largest population​
+// 3. Create a mix of different Observables, clicks, interval and so on, but only log their result every 3 seconds​.
 
-combineLatest(
-  Array.from(document.getElementsByClassName('country')).map(
-    (data: HTMLInputElement) =>
-      fromEvent(data, 'input').pipe(
-        debounceTime(500),
-        filter((data) => data.target.value),
-        switchMap((data) =>
-          fromFetch(
-            `https://restcountries.com/v3.1/name/${data.target.value}?fullText=true`
-          ).pipe(
-            switchMap((response) => {
-              return response.json();
-            }),
-            filter((item) => item.length),
-            map((item) => item[0])
-          )
-        )
-      )
-  )
-).subscribe((data) => {
-  const countryWithLargestPopulation = data.find(
-    (item) =>
-      item.population ===
-      Math.max.apply(
-        null,
-        data.map((c) => c.population)
-      )
-  );
-  console.log(
-    countryWithLargestPopulation.altSpellings[1] +
-      ' : ' +
-      countryWithLargestPopulation.population
-  );
-});
+merge(fromEvent(document, 'dblclick'), interval(1500))
+  .pipe(auditTime(3000))
+  .subscribe(console.log);
